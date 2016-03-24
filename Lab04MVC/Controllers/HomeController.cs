ef6ebc56aa322b7ae8ec65ab8deb121b3f1374ef
Brad Florence <florencb@go.stockton.edu>
2016-03-24 13:57:34 -0400

    initial commit

---
+++ Lab04MVC/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using Lab04MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab04MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Test2()
+        {
+            
+            DateTime date = DateTime.Now;
+            ViewBag.Greeting = "Good Afternoon ";
+            ViewBag.Greeting += "The time is " + date.ToShortTimeString();
+            ViewBag.Greeting += ", and the date is " + date.ToLongDateString();
+            ViewBag.Greeting += ", which is day " + date.DayOfYear;
+
+      
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Test()
+        {
+            Person p = new Person { FirstName = "Joe", LastName = "Wallice", BirthDate = new DateTime(1980, 1, 10) };
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult Test(Person newPerson)
+        {
+            bool ok = ModelState.IsValid;
+            return View(newPerson);
+        }
+    }
+}
